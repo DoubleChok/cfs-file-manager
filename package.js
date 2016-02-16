6d9f4e1f133c78020b5a8d4b1368408c3406b34f
Daniel Bernhard <me@danielbernhard.com>
2016-02-16 17:41:11 -0500

    update packagejs

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'dbernhard:cfs-file-manager',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Graphic file manager for meteor collectionFS package',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/huevoncito/cfs-file-manager',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
